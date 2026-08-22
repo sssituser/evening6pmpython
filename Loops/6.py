bdc56960b7230f3e798dd1b80030deda7c52b86d
arunkumar <allamarunkumar@gmail.com>
2026-08-22 18:08:29 +0530

    loops

---
+++ Loops/6.py
@@ -0,0 +1,13 @@
+# start=int(input('Enter a number : '))
+# end = 2
+
+# while start>=end:
+#     print(start,end=" ")
+#     start -= 2
+
+start = int(input('Enter a number : '))
+end = 3
+while start>=end:
+    print(start,end=" ")
+
+    start-=3
---
+++ Loops/programs.py
@@ -0,0 +1,19 @@
+'''
+1. Write program to generate number 1 to given number:
+input 5: outut  1 2 3 4 5
+
+2. Write a program to find the sum of n number for the given number?
+input 5: sum = 1+2+3+4+5
+
+3.Write a program to find the factorial of given number ?
+ input : 5   Fact : 1*2*3*4*5=>120
+
+4.Separate the digits of a given number ?
+ input : 123   output :  3   2   1
+ input : 456   output :  6   5   4
+
+5.Counting the digits of a given number
+input : 123       Digits : 3
+input : 4567      Digit  : 4
+
+'''
